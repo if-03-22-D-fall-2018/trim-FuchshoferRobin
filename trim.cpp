7b263fa7366950c7ffccc088ff6310ee7aa261ab
FuchshoferRobin <robinfuchshofer@gmail.com>
2018-12-18 10:34:09 -0800

    no errors left

--- trim.cpp
+++ trim.cpp
@@ -10,3 +10,39 @@
  * Test functions for trim.h
  * ----------------------------------------------------------
  */
+#include "trim.h"
+#include <string.h>
+#include "stdio.h"
+
+void 	trim (const char *source, char *trimmed_string)
+{
+  if(strcmp(source,"") == 0)
+  {
+    strcpy(trimmed_string,"");
+  }
+  else
+  {
+    int minFree = 0;
+    int maxFree = strlen(source)-1;
+    int counter = 0;
+    while(source[minFree] == ' ')
+    {
+      minFree+=1;
+    }
+    while (maxFree >= minFree && source[maxFree] == ' ') {
+      maxFree-=1;
+    }
+
+
+    for (int i = minFree; i < maxFree+1; i++) {
+      trimmed_string[counter] = source[i];
+      counter+=1;
+    }
+
+
+  }
+
+
+
+
+}
